73526ad53c98621c377485151791cfb2fb87a715
Yora <13516067@std.stei.itb.ac.id>
2019-04-12 15:33:00 +0700

    edit problem2.py

--- Problem 2/problem2.py
+++ Problem 2/problem2.py
@@ -2,6 +2,7 @@
 
 
 def main():
+    n = int(input("Input nilai N : "))
     n1 = 1
     n2 = 2
 
@@ -10,25 +11,16 @@ def main():
     result = 2
     nf = 0
 
-    while (nf<4000000):
+    while (nf<n):
         nf = n1 + n2
         n1 = n2
         n2 = nf
-        if(nf<4000000 and nf%2==0):
+        if(nf<n and nf%2==0):
             data.append(nf)
             result += nf
         nf+=1
-    print(nf)
     
-    dataLength = len(data)
-    print("angka fibo :")
-    for i in range(dataLength):
-        print(data[i],end = " ")
-    
-    print("\nsum :" , result)
-
-    
-
+    print("sum :" , result)
 
 if __name__ == "__main__":
     main()
